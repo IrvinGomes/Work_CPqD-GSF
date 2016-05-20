b755b8b6569a30260614e2d3d169fc1846b953cc
Irvin 'Raneki' Gomes <irvin.r.gomes@gmail.com>
2016-05-20 15:23:49 -0300

    melhoria teste multi UE

--- fapi/multUE/teste_leitura_multUE.py
+++ fapi/multUE/teste_leitura_multUE.py
@@ -11,6 +11,12 @@ udp.bind(local)
 while True:
   leitor, recebe = udp.recvfrom(65000)
 
-  msg_Id, len_Ven, buff_Length = unpack('>BBH', leitor[0:4])
+  msg_Id, len_Ven, buff_Length, frame = unpack('>BBHH', leitor[0:6])
 
   print msg_Id, len_Ven, buff_Length
+
+if msg_Id is 0x87:
+
+  num_of_pdu = unpack('>H', leitor[7:8])
+
+  print num_of_pdu
