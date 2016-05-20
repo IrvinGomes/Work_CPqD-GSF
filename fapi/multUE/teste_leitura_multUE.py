b8292de90ab43c439bd14ba520bb5ebef993a961
Irvin 'Raneki' Gomes <irvin.r.gomes@gmail.com>
2016-05-20 15:57:36 -0300

    melhoria na leitura para multi ue

--- fapi/multUE/teste_leitura_multUE.py
+++ fapi/multUE/teste_leitura_multUE.py
@@ -13,10 +13,13 @@ while True:
 
   msg_Id, len_Ven, buff_Length, frame = unpack('>BBHH', leitor[0:6])
 
-  print msg_Id, len_Ven, buff_Length
 
 if msg_Id is 0x87:
 
-  num_of_pdu = unpack('>H', leitor[7:8])
+  print msg_Id, len_Ven, buff_Length
+  num_of_pdu = unpack('>H', leitor[6:8])
 
   print num_of_pdu
+
+  rnti = unpack('>H', leitor[12:14])
+  print rnti
